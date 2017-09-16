32c0baab1488140eef58ed9b675a7225a47bf37a
MOZART\Vivaldi <leon.sutedja@gmail.com>
2017-09-17 09:11:32 +1000

    Initial Commit

---
+++ DefaultWindowsService/DefaultService.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using NLog;
+using System;
+using System.ServiceProcess;
+
+namespace DefaultWindowsService
+{
+    public class MessageLog
+    {
+        public string Message { get; set; }
+        public string ErrorMessage { get; set; }
+        public DateTime Received { get; set; }
+    }
+
+    partial class DefaultService : ServiceBase
+    {
+        private readonly static Logger _logger = LogManager.GetCurrentClassLogger();
+
+        System.Timers.Timer timeDelay;
+        int count;
+        public DefaultService()
+        {
+            InitializeComponent();
+            timeDelay = new System.Timers.Timer();
+            timeDelay.Elapsed += new System.Timers.ElapsedEventHandler(WorkProcess);
+        }
+
+        public void WorkProcess(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            string process = "Timer Tick " + count;
+            var message = new MessageLog()
+            {
+                Message = process,
+                ErrorMessage = string.Empty,
+                Received = DateTime.Now
+            };
+            LogService(message);
+            count++;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            var message = new MessageLog()
+            {
+                Message = "Service Started",
+                ErrorMessage = string.Empty,
+                Received = DateTime.Now
+            };
+            LogService(message);
+            timeDelay.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            var message = new MessageLog()
+            {
+                Message = "Service Stopped",
+                ErrorMessage = string.Empty,
+                Received = DateTime.Now
+            };
+            LogService(message);
+            timeDelay.Enabled = false;
+        }
+
+        private void LogService(MessageLog messageLog)
+        {
+            var serializedMessage = JsonConvert.SerializeObject(messageLog);
+            _logger.Debug(serializedMessage);
+        }     
+    }
+}
---
+++ DefaultWindowsService/ProjectInstaller.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace DefaultWindowsService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.DefaultServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // DefaultServiceInstaller
+            // 
+            this.DefaultServiceInstaller.ServiceName = "Default Windows Service";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.DefaultServiceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller DefaultServiceInstaller;
+    }
+}
